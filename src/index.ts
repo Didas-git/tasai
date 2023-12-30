d137d386095d6898f6462052c029706a7c3b0052
DidaS <didasoficial@gmail.com>
2023-12-30 03:49:44 +0000

    Added 8bit colors and macro

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { parseUsing4BitColors } from "./macros/4bit.macro.js";
+import { parseUsing8BitColors } from "./macros/8bit.macro.js";
 
-console.log(parseUsing4BitColors("<bg_black>hello <bright_red>world<r> or <cyan>Dave<r><r>"));
+console.log(parseUsing8BitColors("<bg_black>hello <bright_red>world<r> or <cyan>Dave<r> and <87>8bits<r><r>"));
---
+++ src/macros/8bit.macro.ts
@@ -0,0 +1,42 @@
+import { colorizeWith8Bit, get8BitColorNormalizer } from "../colors/8bit.js";
+import {
+    ForegroundColorMap,
+    BackgroundColorMap,
+    colorizeWith4Bit
+} from "../colors/4bit.js";
+
+export function parseUsing8BitColors(string: string, isBackground: boolean = false): string {
+    const opening = string.indexOf("<");
+    if (opening === -1) return string;
+
+    const closing = string.indexOf(">", opening);
+    if (closing === -1) throw new Error("Incorrect syntax");
+
+    const color = string.substring(opening + 1, closing).toLowerCase();
+    const noColorPartBeginning = string.slice(0, opening);
+
+    const afterColor = string.substring(closing + 1);
+    if (!afterColor.length || afterColor === "<r>") return noColorPartBeginning;
+
+    if (color === "r") return `${noColorPartBeginning}${get8BitColorNormalizer(isBackground)}${parseUsing8BitColors(afterColor)}`;
+    if (color.startsWith("#")) throw new Error("Hex colors are not supported when using 8bit colors");
+
+    if (color.startsWith("bg")) {
+        const parsedColor = color.replace("bg_", "");
+        return `${noColorPartBeginning}${colorize(afterColor, parsedColor, true)}`;
+    }
+
+    return `${noColorPartBeginning}${colorize(afterColor, color)}`;
+}
+
+function colorize(string: string, colorCodeOrName: string, isBackground: boolean = false): string {
+    const colorCode = parseInt(colorCodeOrName);
+
+    if (Number.isNaN(colorCode)) {
+        const code = isBackground ? BackgroundColorMap[colorCodeOrName] : ForegroundColorMap[colorCodeOrName];
+        if (typeof code === "undefined") throw new Error(`Unknown color '${colorCodeOrName}'`);
+        return colorizeWith4Bit(parseUsing8BitColors(string, isBackground), code);
+    }
+
+    return colorizeWith8Bit(parseUsing8BitColors(string, isBackground), colorCode);
+}
